0d375fbbd6b3be586a2908da2ca925f4d5578dc2
LeinardB92 <boanergespife@gmail.com>
2023-09-07 13:49:17 -0600

    Variables experiodes desde el servicio auth.service.ts y creaciòn de primera peticiòn http. Uso del mètodo login() de este servicio a el componente login-page.component.ts

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +14,30 @@ export class AuthService {
 
   // constructor(private http : HttpClient) { }
 
-  private _currentUser = signal<User | null>(null)
-  private _authStatus = signal<AuthStatus>(AuthStatus.checking)
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-  login(emai:string, password:string): Observable<boolean>{
-    return of(true)
+  //! Al mundo exterior
+  public currentUser = computed(() => this._currentUser);
+  public authStatus = computed(() => this._authStatus);
+
+  private setAuthentication(user: User, token: string) {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    console.log({user, token});
+
+    return true;
+  };
+
+  public login(email:string, password:string): Observable<boolean>{
+    const url = `${this.baseUrl}/auth/loging`;
+    const body = {email, password}
+
+    return this.http.post<LoginResponse>(url, body)
+    .pipe(
+      map(({user, token}) => this.setAuthentication(user, token)),
+      catchError(err => throwError(() => err.error.message))
+      );
   }
 }
