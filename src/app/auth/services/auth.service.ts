e9cca130ccfa868770a5d2bc14162cb3d22df846
LeinardB92 <boanergespife@gmail.com>
2023-09-07 20:58:30 -0600

    creación de método checkAuthStatus y la interface CheckTokenResponse, para verificar la conexión del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { Observable, catchError, map, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
 import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { CheckTokenResponse } from '../interfaces/checkToken.response';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,8 @@ export class AuthService {
   private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-  //! Al mundo exterior
+  //! Al mundo exterior -----------------------------------------------
+
   public currentUser = computed(() => this._currentUser());
   public authStatus = computed(() => this._authStatus());
 
@@ -30,6 +32,7 @@ export class AuthService {
     return true;
   };
 
+
   public login(email:string, password:string): Observable<boolean>{
     const url = `${this.baseUrl}/auth/loging`;
     const body = {email, password}
@@ -40,4 +43,25 @@ export class AuthService {
       catchError(err => throwError(() => err.error.message))
       );
   }
+
+
+  public checkAuthStatus() : Observable<boolean>{
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if(!token){
+      return of(false)
+    };
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${ token }`);
+
+    return this.http.get<CheckTokenResponse>(url, {headers})
+    .pipe(
+      map( ({user, token})  => this.setAuthentication( user, token )),
+      catchError(() => {this._authStatus.set(AuthStatus.notAuthenticated);
+      return of(false);
+      })
+    );
+  }
+
 }
